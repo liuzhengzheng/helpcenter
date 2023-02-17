54d5d9086f5f8f50abdd58bab78b8f7c2db98db8
liuzhengzheng <liuzhengzheng@kuaishou.com>
2023-02-17 21:12:29 +0800

    init

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<button on:click={handleClick} class="button">
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+
+<style>
+	.button{
+			background-color: red;
+	}
+</style>
